d82f87ada1852e611c548222bcf5ea08882258aa
WayneLin1992 <62940642+WayneLin1992@users.noreply.github.com>
2021-04-06 20:18:23 +0800

    fix the vector.c
    fix the vector.c

--- vector.c
+++ vector.c
@@ -1,131 +1,48 @@
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+#include "vector.h"
 
-/* Display prompt */
-static void prompt()
-{
-    write(2, "$ ", 2);
-}
-
-/* Display error message, optionally - exit */
-static void fatal(int retval, int leave)
-{
-    if (retval >= 0)
-        return;
-    write(2, "?\n", 2);
-    if (leave)
-        exit(1);
-}
-
-/* Helper functions to detect token class */
-static inline int is_delim(int c)
-{
-    return c == 0 || c == '|';
-}
-
-static inline int is_redir(int c)
-{
-    return c == '>' || c == '<';
-}
-
-static inline int is_blank(int c)
-{
-    return c == ' ' || c == '\t' || c == '\n';
-}
-
-static int is_special(int c)
-{
-    return is_delim(c) || is_redir(c) || is_blank(c);
-}
-
-/* Recursively run right-most part of the command line printing output to the
- * file descriptor @t
- */
-static void run(char *c, int t)
-{
-    char *redir_stdin = NULL, *redir_stdout = NULL;
-    int pipefds[2] = {0, 0}, outfd = 0;
-    char *v[99] = {0};
-    char **u = &v[98]; /* end of words */
-    for (;;) {
-        c--;
-        if (is_delim(*c)) /* if NULL (start of string) or pipe: break */
-            break;
-        if (!is_special(*c)) {
-            c++;    /* Copy word of regular chars into previous u */
-            /* 在此提交你的程式碼 */
-        }
-        if (is_redir(*c)) { /* If < or > */
-            if (*c == '<')
-                redir_stdin = *u;
-            else
-                redir_stdout = *u;
-            if ((u - v) != 98)
-                u++;
-        }
-    }
-    if ((u - v) == 98) /* empty input */
-        return;
-
-    if (!strcmp(*u, "cd")) { /* built-in command: cd */
-        fatal(chdir(u[1]), 0);
-        return; /* actually, should run() again */
-    }
-
-    if (*c) {
-        pipe(pipefds);
-        outfd = pipefds[1]; /* write end of the pipe */
-    }
-
-    pid_t pid = fork();
-    if (pid) { /* Parent or error */
-        fatal(pid, 1);
-        if (outfd) {
-            run(c, outfd);     /* parse the rest of the cmdline */
-            close(outfd);      /* close output fd */
-            close(pipefds[0]); /* close read end of the pipe */
-        }
-        wait(0);
-        return;
-    }
-
-    if (outfd) {
-        dup2(pipefds[0], 0); /* dup read fd to stdin */
-        close(pipefds[0]);   /* close read fd */
-        close(outfd);        /* close output */
-    }
-
-    if (redir_stdin) {
-        close(0); /* replace stdin with redir_stdin */
-        fatal(open(redir_stdin, 0), 1);
-    }
-
-    if (t) {
-        dup2(t, 1); /* replace stdout with t */
-        close(t);
-    }
-
-    if (redir_stdout) {
-        close(1);
-        fatal(creat(redir_stdout, 438), 1); /* replace stdout with redir */
-    }
-    fatal(execvp(*u, u), 1);
-}
 
 int main()
 {
-    while (1) {
-        prompt();
-        char buf[512] = {0}; /* input buffer */
-        char *c = buf;
-        if (!fgets(c + 1, sizeof(buf) - 1, stdin))
-            exit(0);
-        for (; *++c;) /* skip to end of line */
-            ;
-        run(c, 0);
-    }
+    v(float, 2, vec1);
+    v(int, 2, vec2, 13, 42);
+    printf("pos(vec2,0)=%d, pos(vec2,1)=%d\n", vec_pos(vec2, 0),
+        vec_pos(vec2, 1));
+    vec_push_back(vec2, 88);
+    vec_reserve(vec2, 100);
+    printf("capacity(vec1)=%zu, capacity(vec2)=%zu\n", vec_capacity(vec1),
+           vec_capacity(vec2));
+    printf("pos(vec2,2)=%d\n", vec_pos(vec2, 2));
+    #define display(v)                               \
+    do {                                         \
+        for (size_t i = 0; i < vec_size(v); i++) \
+            printf("%.2f ", vec_pos(v, i));      \
+        puts(v.on_heap ? "heap" : "stack");      \
+    } while (0)
+
+    display(vec1);
+    vec_push_back(vec1, 0.0);
+    display(vec1);
+    vec_push_back(vec1, 1.1);
+    display(vec1);
+    vec_push_back(vec1, 2.2);
+    display(vec1);
+    vec_push_back(vec1, 3.3);
+    display(vec1);
+    vec_push_back(vec1, 4.4);
+    display(vec1);
+    vec_push_back(vec1, 5.5);
+    display(vec1);
+    vec_pop_back(vec1);
+    display(vec1);
+    vec_pop_back(vec1);
+    display(vec1);
+    vec_pop_back(vec1);
+    display(vec1);
+    vec_pop_back(vec1);
+    display(vec1);
+    vec_pop_back(vec1);
+    display(vec1);
+    vec_pop_back(vec1);
+    display(vec1);
     return 0;
 }
