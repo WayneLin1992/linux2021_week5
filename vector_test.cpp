cb3d4b3e1535e0d5baa8f63e8d0aa4a85a151d01
WayneLin1992 <62940642+WayneLin1992@users.noreply.github.com>
2021-04-07 23:40:16 +0800

    Add std::vector testbench
    Add std::vector testbench

---
+++ vector_test.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    vector<float> vec1;
+//    cout << vec1.capacity() <<endl;
+//    vector<int>vec2{13,42};
+//    cout << vec2.capacity() <<endl;
+//    vec2.push_back(88);
+//    vec2.reserve(100);
+//    cout << vec2.capacity() <<endl;
+//    for (auto i = vec1.begin(); i != vec1.end(); ++i)
+//        cout << *i << " ";
+//    cout << "\n";
+    vec1.push_back(0.0);
+//    for (auto i = vec1.begin(); i != vec1.end(); ++i)
+//        cout << *i << " ";
+//    cout << "\n";
+    vec1.push_back(1.1);
+//    for (auto i = vec1.begin(); i != vec1.end(); ++i)
+//        cout << *i << " ";
+//    cout << "\n";
+    vec1.push_back(2.2);
+//    for (auto i = vec1.begin(); i != vec1.end(); ++i)
+//        cout << *i << " ";
+//    cout << "\n";
+    vec1.push_back(3.3);
+//    for (auto i = vec1.begin(); i != vec1.end(); ++i)
+//        cout << *i << " ";
+//    cout << "\n";
+    vec1.push_back(4.4);
+//    for (auto i = vec1.begin(); i != vec1.end(); ++i)
+//        cout << *i << " ";
+//    cout << "\n";
+    vec1.push_back(5.5);
+//    cout << vec1.capacity() <<endl;
+//    for (auto i = vec1.begin(); i != vec1.end(); ++i)
+//        cout << *i << " ";
+//    cout << "\n";
+    vec1.pop_back();
+//    for (auto i = vec1.begin(); i != vec1.end(); ++i)
+//        cout << *i << " ";
+//    cout << "\n";
+    vec1.pop_back();
+//    for (auto i = vec1.begin(); i != vec1.end(); ++i)
+//        cout << *i << " ";
+//    cout << "\n";
+    vec1.pop_back();
+//    for (auto i = vec1.begin(); i != vec1.end(); ++i)
+//        cout << *i << " ";
+//    cout << "\n";
+    vec1.pop_back();
+//    for (auto i = vec1.begin(); i != vec1.end(); ++i)
+//        cout << *i << " ";
+//    cout << "\n";
+    vec1.pop_back();
+//    for (auto i = vec1.begin(); i != vec1.end(); ++i)
+//        cout << *i << " ";
+//    cout << "\n";
+    vec1.pop_back();
+//    for (auto i = vec1.begin(); i != vec1.end(); ++i)
+//        cout << *i << " ";
+//    cout << "\n";
+    return  0;
+    }
