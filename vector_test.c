ef5fba0e8fc5a2e5b0f5c0fb0d298a4eb8c1d878
WayneLin1992 <62940642+WayneLin1992@users.noreply.github.com>
2021-04-11 16:36:26 +0800

    Compare the 2^n and 4*2^1.5
    Compare the 2^n and 4*2^1.5

--- vector_test.c
+++ vector_test.c
@@ -1,19 +1,24 @@
-#include "vector.h"
+//#include "vector.h"
+#include "vector_ilog.h"
 
 int main()
 {
+    srand(NULL);
     v(float, 2, vec1);
-    vec_push_back(vec1, 0.0);
-    vec_push_back(vec1, 1.1);
-    vec_push_back(vec1, 2.2);
-    vec_push_back(vec1, 3.3);
-    vec_push_back(vec1, 4.4);
-    vec_push_back(vec1, 5.5);
-    vec_pop_back(vec1);
-    vec_pop_back(vec1);
-    vec_pop_back(vec1);
-    vec_pop_back(vec1);
-    vec_pop_back(vec1);
-    vec_pop_back(vec1);
+//    vec_push_back(vec1, 0.0);
+//    vec_push_back(vec1, 1.1);
+//    vec_push_back(vec1, 2.2);
+//    vec_push_back(vec1, 3.3);
+//    vec_push_back(vec1, 4.4);
+//    vec_push_back(vec1, 5.5);
+    for(int i = 0; i<100; i++){
+        vec_push_back(vec1,rand()%1024);
+    }
+//    vec_pop_back(vec1);
+//    vec_pop_back(vec1);
+//    vec_pop_back(vec1);
+//    vec_pop_back(vec1);
+//    vec_pop_back(vec1);
+//    vec_pop_back(vec1);
     return 0;
 }
